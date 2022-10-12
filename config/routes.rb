98078b01216002eb2c7e8c8bda075bcd2dda9fd5
Luan Monteiro <luancorreamonteiro@gmail.com>
2022-10-12 03:33:02 -0300

    fix: update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'users' 
+
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   resources :exams
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
